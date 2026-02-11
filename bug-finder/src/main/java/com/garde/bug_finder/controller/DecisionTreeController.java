970cd0c6ba1c94da010f77dc275cb338c6ddf280
A-duckling <darrienwhat@gmail.com>
2026-02-10 21:20:36 -0500

    failed, revert this

--- bug-finder/src/main/java/com/garde/bug_finder/controller/DecisionTreeController.java
+++ bug-finder/src/main/java/com/garde/bug_finder/controller/DecisionTreeController.java
@@ -10,11 +10,16 @@ import java.util.Map;
 @RequestMapping("/api/tree")
 @CrossOrigin
 public class DecisionTreeController {
-    private final DecisionTreeService service;
+    private  DecisionTreeService service;
 
     public DecisionTreeController(DecisionTreeService services) 
     {
         this.service = services;
+    }
+
+    @GetMapping("/{id}")
+    public DecisionNode getNode(@PathVariable String id) {
+        return service.getNode(id);
     }
 
     @GetMapping("/start")
@@ -29,4 +34,57 @@ public class DecisionTreeController {
         String nextId = payload.get("next");
         return service.getNode(nextId);
     }
+
+    /*
+    "start": {
+    "id": "start",
+    "question": "What does the insect look most similar to?",
+    "options": [
+      { "answer": "Beetle", "next": "Beetle Question" },
+      { "answer": "Worms", "next": "Worm Question" },
+      { "answer": "Assassin Bug", "next": "Worm Question" },
+      { "answer": "Aphids", "next": "Worm Question" },
+      { "answer": "Slugs", "next": "Worm Question" }
+    ]
+  },
+  "Beetle Question": {
+    "id": "Beetle Question",
+    "question": "Looks most similar to?",
+    "options": [
+      { "answer": "Ladybugs", "next": "Ladybug" },
+      { "answer": "Ground Beetles/Other", "next": "Ground Beetle Question" }
+    ]
+  },
+
+  "Ground Beetle Question": {
+    "id": "Ground Beetle Question",
+    "question": "Looks most similar to?",
+    "options": [
+      {"answer": "Ground Beetle", "next": "Ground Beetle"},
+      {"answer": "Japanese Beetle", "next": "Japanese Beetle"},
+      {"answer": "Cucumber Beetle", "next": "Cucumber Beetle"}
+    ]
+  },
+  "Ladybug": {
+  "id": "Ladybug",
+  "bug": {
+    "name": "Ladybug",
+    "classification": "Beneficial",
+    "description": "Ladybugs eat aphids and protect plants.",
+    "images": [],
+    "affectsPlants": ["Roses", "Tomatoes"],
+    "eats": ["Aphids"]
+  }
+},
+    "Ground Beetle": {
+    "id": "Ground Beetle",
+    "bug": {
+        "name": "Ground Beetle",
+        "classification": "Beneficial",
+        "description": "Ground Beetles smt.",
+        "images": ["images/ground-beetle.jpg"],
+        "affectsPlants": ["Flower", "Tomatoes"],
+        "eats": ["Bugs"]
+    }
+    } */
 }
